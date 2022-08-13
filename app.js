f85f24ac8370592f9fe6b1b9b36f4ed2e333195f
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-13 14:43:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-btn')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+
+
+// Function
+function addTodo(event){
+  //Prevent form from submitting
+  event.preventDefault()
+  //Todo DIV
+  const todoDiv = document.createElement('div')
+  todoDiv.classList.add("todo")
+  //Create LI
+  const newTodo = document.createElement('li')
+  newTodo.innerText = todoInput.value
+  newTodo.classList.add('todo-item')
+  todoDiv.appendChild(newTodo)
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement('button')
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  completedButton.classList.add("complete-btn")
+  todoDiv.appendChild(completedButton)
+  // CHECK TRASH BUTTON
+  const trashButton = document.createElement('button')
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add("trash-btn")
+  todoDiv.appendChild(trashButton)
+  // APPEND TO LIST 
+  todoList.appendChild(todoDiv)
+  // Clear Todo INPUT VALUE
+  todoInput.value = ''
+}
+
+
+
